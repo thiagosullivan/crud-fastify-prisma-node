0f80ed1f6469ecb53159072cd88218960a433847
thiagosullivan <thiagosullivan12@gmail.com>
2025-10-29 01:21:03 -0300

    fix: resolving prisma problems and add fastify

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import fastify, { type FastifyInstance } from "fastify";
+
+const app: FastifyInstance = fastify();
+
+app.listen(
+  {
+    port: 3100,
+  },
+  () => console.log("Server is running on port 3100")
+);
